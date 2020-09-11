96c96a37405909d2acdabc42d63d2c14426d6a87
Xerogeiger <gamegenuity@gmail.com>
2020-09-11 14:46:55 -0500

    Fixed InsertionSort implementation, I didn't realize the variable key was used as a temp variable and a check.

--- src/com/ajax/brain/utils/sorts/InsertionSort.java
+++ src/com/ajax/brain/utils/sorts/InsertionSort.java
@@ -26,7 +26,8 @@ public final class InsertionSort<T> extends Sort<T> {
     public void sort(T[] arr) {
         int n = arr.length;
         for (int i = 1; i < n; ++i) {
-            int key = representor.convert(arr[i]);
+            T temp = arr[i];
+            int key = representor.convert(temp);
             int j = i - 1;
 
             /* Move elements of arr[0..i-1], that are
@@ -34,9 +35,9 @@ public final class InsertionSort<T> extends Sort<T> {
                of their current position */
             while (j >= 0 && representor.convert(arr[j]) > key) {
                 arr[j + 1] = arr[j];
-                j = j - 1;
+                j -= 1;
             }
-            arr[j + 1] = arr[i];
+            arr[j + 1] = temp;
         }
     }
 
