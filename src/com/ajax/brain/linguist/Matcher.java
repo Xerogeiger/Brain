af6a182e89727da3c8fc444bb403d09bfecb82e8
Xerogeiger <gamegenuity@gmail.com>
2020-09-16 02:00:02 -0500

    Added more types of Matchers to Matcher.java

--- src/com/ajax/brain/linguist/Matcher.java
+++ src/com/ajax/brain/linguist/Matcher.java
@@ -1,5 +1,8 @@
 package com.ajax.brain.linguist;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 /**
  * Says if the string is a match
  */
@@ -140,6 +143,110 @@ public abstract class Matcher {
     }
 
     /**
+     * Creates a new {@code MultiCharMatcher} from the provided values
+     *
+     * @param priority the priority of the {@code Matcher}
+     * @param type the type of token this {@code Matcher} finds
+     * @param chars the {@code String}s of characters this {@code Matcher} should match
+     * @return the new {@code MultiCharacterMatcher}
+     */
+    public static Matcher getMultiCharacterMatcher(int priority, TokenType type, String chars) {
+        return new MultiCharacterMatcher(priority, type, chars);
+    }
+
+    /**
+     * Creates a new {@code MultiCharMatcher} from the provided values
+     *
+     * @param priority the priority of the {@code Matcher}
+     * @param type the type of token this {@code Matcher} finds
+     * @param chars the characters this {@code Matcher} should match
+     * @return the new {@code MultiCharacterMatcher}
+     */
+    public static Matcher getMultiCharacterMatcher(int priority, TokenType type, char... chars) {
+        return new MultiCharacterMatcher(priority, type, chars);
+    }
+
+    /**
+     * Creates a new {@code RegexMatcher} from the provided values
+     *
+     * @param priority the priority of the {@code Matcher}
+     * @param type the type of token this {@code Matcher} finds
+     * @param characterMatch whether or not this {@code Matcher} matches characters
+     * @param pattern the Regex pattern this {@code Matcher} should match with
+     * @return the new {@code RegexMatcher}
+     */
+    public static Matcher getRegexMatcher(int priority, TokenType type, boolean characterMatch, String pattern) {
+        return new RegexMatcher(priority, type, characterMatch, pattern);
+    }
+
+    /**
+     * Creates a new {@code RegexMatcher} from the provided values
+     *
+     * @param priority the priority of the {@code Matcher}
+     * @param type the type of token this {@code Matcher} finds
+     * @param characterMatch whether or not this {@code Matcher} matches characters
+     * @param pattern the Regex pattern this {@code Matcher} should match with
+     * @return the new {@code RegexMatcher}
+     */
+    public static Matcher getRegexMatcher(int priority, TokenType type, boolean characterMatch, Pattern pattern) {
+        return new RegexMatcher(priority, type, characterMatch, pattern);
+    }
+
+    /**
+     * Creates a new {@code DigitMatcher} from the provided values
+     *
+     * @param priority the priority of the {@code Matcher}
+     * @param type the type of token this {@code Matcher} finds
+     * @return the new {@code DigitMatcher}
+     */
+    public static Matcher getDigitMatcher(int priority, TokenType type) {
+        return new DigitMatcher(priority, type);
+    }
+
+    /**
+     * Matcher that uses Java Regex
+     */
+    private static class RegexMatcher extends Matcher {
+        private final Pattern pattern;
+
+        /**
+         * Creates a new {@code RegexMatcher} with the provided values
+         *
+         * @param priority       the priority of the matcher
+         * @param type           the type of token this matcher finds
+         * @param characterMatch whether or not this matcher matches characters
+         * @param pattern the regex pattern to match with
+         */
+        public RegexMatcher(int priority, TokenType type, boolean characterMatch, String pattern) {
+            this(priority, type, characterMatch, Pattern.compile(pattern));
+        }
+
+        /**
+         * Creates a new {@code RegexMatcher} with the provided values
+         *
+         * @param priority       the priority of the matcher
+         * @param type           the type of token this matcher finds
+         * @param characterMatch whether or not this matcher matches characters
+         * @param pattern the regex pattern to match with
+         */
+        public RegexMatcher(int priority, TokenType type, boolean characterMatch, Pattern pattern) {
+            super(priority, type, characterMatch);
+            this.pattern = pattern;
+        }
+
+        /**
+         * Matches the stored regex value to the provided text
+         *
+         * @param text the value to check
+         * @return if the value matched
+         */
+        @Override
+        public boolean match(String text) {
+            return pattern.matcher(text).find();
+        }
+    }
+
+    /**
      * Matches single characters
      */
     private static class CharacterMatcher extends Matcher {
@@ -173,6 +280,7 @@ public abstract class Matcher {
 
         /**
          * Matches the stored value to the provided text
+         *
          * @param text the value to check
          * @return if the value matched
          */
@@ -180,6 +288,52 @@ public abstract class Matcher {
         public boolean match(String text) {
             if(text.length() == 1) {
                 return text.charAt(0) == this.match;
+            }
+            return false;
+        }
+    }
+
+    /**
+     * Matches multiple characters
+     */
+    private static class MultiCharacterMatcher extends Matcher {
+        private final char[] match;
+
+        /**
+         * Creates a new {@code MultiCharacterMatcher} with the {@code String} to match and priority
+         *
+         * @param chars the {@code String} of characters to match
+         * @param priority the match priority
+         * @param type the type of token the match would be
+         */
+        public MultiCharacterMatcher(int priority, TokenType type, String chars) {
+            super(priority, type, true);
+            this.match = chars.toCharArray();
+            Arrays.sort(this.match);
+        }
+
+        /**
+         * Creates a new {@code MultiCharacterMatcher} with the {@code String} to match and priority
+         *
+         * @param match the characters to match
+         * @param priority the match priority
+         * @param type the type of token the match would be
+         */
+        public MultiCharacterMatcher(int priority, TokenType type, char... match) {
+            super(priority, type, true);
+            this.match = match.clone();
+            Arrays.sort(this.match);
+        }
+
+        /**
+         * Matches the stored value to the provided text
+         * @param text the value to check
+         * @return if the value matched
+         */
+        @Override
+        public boolean match(String text) {
+            if(text.length() == 1) {
+                return Arrays.binarySearch(this.match, text.charAt(0)) > -1;
             }
             return false;
         }
@@ -328,6 +482,45 @@ public abstract class Matcher {
 
             for(int x = 0; x < text.length(); x++) {
                 if(!Character.isWhitespace(text.charAt(x)))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+
+    /**
+     * {@code Matcher} for digits
+     */
+    private static class DigitMatcher extends Matcher {
+        /**
+         * Creates a new {@code DigitMatcher}
+         * @param priority the priority of the digit {@code DigitMatcher}
+         * @param tokenType the token type of the matched digits
+         */
+        public DigitMatcher(int priority, TokenType tokenType) {
+            super(priority, tokenType, false);
+        }
+
+        /**
+         * Returns whether or not the provided {@code String} is only digits
+         *
+         * @param text the text to match
+         * @return {@code false} if the text length is 0 or not entirely digits
+         */
+        @Override
+        public boolean match(String text) {
+            if(text.length() == 0)
+                return false;
+
+            if(text.length() == 1) {
+                char ws = text.charAt(0);
+
+                return Character.isDigit(ws);
+            }
+
+            for(int x = 0; x < text.length(); x++) {
+                if(!Character.isDigit(text.charAt(x)))
                     return false;
             }
 
