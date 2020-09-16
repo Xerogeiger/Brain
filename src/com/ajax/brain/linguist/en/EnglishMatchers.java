caff7d4f2e0e5d9732ec879a8e1d6bf027cc0da8
Xerogeiger <gamegenuity@gmail.com>
2020-09-16 02:02:32 -0500

    Added a few more Matchers for commas, numbers, and conjunctions

--- src/com/ajax/brain/linguist/en/EnglishMatchers.java
+++ src/com/ajax/brain/linguist/en/EnglishMatchers.java
@@ -11,18 +11,24 @@ public final class EnglishMatchers {
 
     public final Matcher colonMatcher;
     public final Matcher semiColonMatcher;
+    public final Matcher coordinatingConjunctionMatcher;
+    public final Matcher commaMatcher;
+    public final Matcher digitMatcher;
 
     public final Matcher[] allMatchers;
 
     private EnglishMatchers() {
-        this(1);
+        this(1, 3, 1);
     }
 
-    public EnglishMatchers(int symbolPriority) {
+    public EnglishMatchers(int symbolPriority, int conjunctionPriority, int digitPriority) {
         colonMatcher = Matcher.getCharacterMatcher(':', symbolPriority, TokenType.SUMMARY_SEPARATOR);
         semiColonMatcher = Matcher.getCharacterMatcher(';', symbolPriority, TokenType.CLAUSE_SEPARATOR);
+        commaMatcher = Matcher.getCharacterMatcher(',', symbolPriority, TokenType.LIST_SEPARATOR);
+        coordinatingConjunctionMatcher = Matcher.getMultiWordMatcher(conjunctionPriority, TokenType.COORDINATING_CONJUNCTION, true, "and", "or", "but", "for", "so", "yet", "nor");
+        digitMatcher = Matcher.getDigitMatcher(digitPriority, TokenType.NUMBER);
 
-        allMatchers = new Matcher[]{colonMatcher, semiColonMatcher};
+        allMatchers = new Matcher[]{colonMatcher, semiColonMatcher, commaMatcher, coordinatingConjunctionMatcher, digitMatcher};
     }
 
     public Matcher[] getMatchers() {
